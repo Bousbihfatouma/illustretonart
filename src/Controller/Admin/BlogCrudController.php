b6f8c2be6aca59428b94264451bd7d6715afca73
Bousbih <fatoumabousbih@yahoo.fr>
2023-11-01 13:41:49 +0100

    Categoryrelationblog

--- src/Controller/Admin/BlogCrudController.php
+++ src/Controller/Admin/BlogCrudController.php
@@ -3,16 +3,18 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Blog;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
 
 class BlogCrudController extends AbstractCrudController
 {
@@ -22,7 +24,8 @@ class BlogCrudController extends AbstractCrudController
     {
         return Blog::class;
     }
-     public function configureCrud(Crud $crud): Crud
+
+    public function configureCrud(Crud $crud): Crud
     {
         return $crud
             ->setEntityLabelInSingular('blog')
@@ -30,7 +33,7 @@ class BlogCrudController extends AbstractCrudController
 
             ->setPageTitle("index", "illustretonart - Administration blog")
 
-            ->setPaginatorPageSize(20)
+     /*        ->setPaginatorPageSize(20) */
 
             ->addFormTheme('@FOSCKEditor/Form/ckeditor_widget.html.twig');
     }
@@ -40,9 +43,10 @@ class BlogCrudController extends AbstractCrudController
         return [
             IdField::new('id')->onlyOnDetail(),
             TextField::new('image'),
-             TextField::new('title'),  // Ajout du champ 'title'
-             TextEditorField::new('content'),
-             Field::new('category'),
+            BooleanField::new('online'),
+            TextField::new('title'),  // Ajout du champ 'title'
+            TextEditorField::new('content'),
+             AssociationField::new('category'),
 
       
         ];
